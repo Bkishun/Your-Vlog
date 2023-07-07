7db1e2dd72ec5d753d522c95aecfa0acb3539462
Balkishun maurya <bkishun21072001@gmail.com>
2023-07-07 16:43:35 +0530

    .5... adding log out Functionality in our poject

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,16 @@
 import {Link} from "react-router-dom" ;
 import { auth } from "../config/firebase";
 import {useAuthState} from "react-firebase-hooks/auth"
-
+import { signOut } from "firebase/auth";
+import { sign } from "crypto";
 
 export const Navbar = () => {
 
     const[user] = useAuthState(auth);
 
+    const signUserOut = async () => {
+        await signOut(auth);
+    };
     return (
         <div className="navbar">
             <div className="links">
@@ -15,8 +19,13 @@ export const Navbar = () => {
             </div>
 
             <div className="user">
-                <p>{user?.displayName}</p>
-                <img src ={user?.photoURL || ""} width="20" height="20"/>
+                {user && (  //if user not exist it does not show any hadding and imag tag
+                    <>
+                        <p>{user?.displayName}</p>
+                        <img src ={user?.photoURL || ""} width="20" height="20"/>
+                        <button onClick={signUserOut}>Log Out</button>
+                    </>    
+                )}
             </div>
         </div>
     );
