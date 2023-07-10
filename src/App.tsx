ad40d54064a3f894706353d94e0592ed188a4dc7
Balkishun maurya <bkishun21072001@gmail.com>
2023-07-11 00:28:43 +0530

    .9... showing post of user that is posted on database in main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
-import { Main } from "./pages/main";
+import { Main } from "./pages/main/main";
 import { Login } from "./pages/login";
 import { Navbar } from "./components/navbar";
-import {CreatePost} from "./pages/create-post/create-post";
+import { CreatePost } from "./pages/create-post/create-post";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
